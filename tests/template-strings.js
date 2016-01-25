cb176baede5c0cf4883f125ef4742c93e5d198e1
Allen Phelps <allenatmc@gmail.com>
2016-01-25 15:28:14 -0600

    finish template strings tests

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -11,4 +11,29 @@ describe('template strings', function() {
     var string = `string`;
     expect(string).to.be.a('string');
   });
+
+  it('can evaluate variables', function () {
+    var foo = 'bar';
+    var string = `foo${foo}`;
+    expect(string).to.equal('foobar');
+  });
+  
+  it('can execute functions', function () {
+    var string = `The date is: ${new Date()}`;
+    var date = new Date();
+    expect(string).to.equal('The date is: ' + date);
+  });
+  
+  it('can evaluate expressions', function () {
+      var string = `One plus one is: ${1+1}`;
+      expect(string).to.equal('One plus one is: 2');
+    });
+
+  it('can handle multi-line strings', function () {
+      var string = `Line1
+Line2`;
+      expect(string).to.equal(['Line1', 'Line2'].join('\n'));
+    });
+
+
 });
