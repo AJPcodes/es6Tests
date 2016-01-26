893d02def280285e03b8a118a9deb5b9ca39d6d5
Allen Phelps <allenatmc@gmail.com>
2016-01-26 10:30:44 -0600

    move use strict to function scope

--- tests/blockscope.js
+++ tests/blockscope.js
@@ -1,8 +1,9 @@
 //teo tests - use let
-"use strict";
 var expect = require('chai').expect;
 
 describe('blockscope', function(){
+
+"use strict";
 
   it('should have block level variabe scoping', function(){
     var i = 10;
@@ -10,20 +11,15 @@ describe('blockscope', function(){
       let foo = [];
       foo.push(i);
     }
-  
     expect(i).to.equal(10);
-
   });
- 
+
   it('should have block level function scoping', function(){
- 
+
     function fn () {return 'outer'};
-    
     {
         function fn () {return 'inner'};
-    } 
-
+    }
     expect(fn()).to.equal('outer');
-
   });
 });
